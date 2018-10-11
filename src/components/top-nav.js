b7c3d7fe27ddd429952db1a670d7ce73e4122ce9
John Keller <jmkeller3@gmail.com>
2018-10-10 22:45:54 -0600

    finished challenge

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
+import React from "react-redux";
 
-import './top-nav.css';
+import { generateAuralUpdate, restartGame } from "../actions";
 
-export default function TopNav(props) {
+import "./top-nav.css";
+import { connect } from "http2";
+
+export function TopNav(props) {
   return (
     <nav>
       <ul className="clearfix">
         <li>
-          <a 
-            href="#what" 
-            className="what"
-            aria-label="How to play"
-          >
+          <a href="#what" className="what" aria-label="How to play">
             What?
           </a>
         </li>
@@ -20,7 +20,9 @@ export default function TopNav(props) {
             href="#feedback"
             className="new"
             aria-label="Start a new game"
-            onClick={() => props.onRestartGame()}
+            onClick={() =>
+              props.dispatch(restartGame(Math.floor(Math.random() * 100) + 1))
+            }
           >
             + New Game
           </a>
@@ -31,7 +33,7 @@ export default function TopNav(props) {
             /* the `visuallyhidden` class hides an element 
             while leaving it available to screen reader users  */
             className="visuallyhidden focusable status-link"
-            onClick={() => props.onGenerateAuralUpdate()}
+            onClick={() => props.dispatch(generateAuralUpdate())}
           >
             Hear state of game
           </a>
@@ -40,3 +42,5 @@ export default function TopNav(props) {
     </nav>
   );
 }
+
+export default connect()(TopNav);
